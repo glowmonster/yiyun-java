6aba31c5f26c762381577c10e07f9d2f6c1387d3
GlowMonster <34387623+glowmonster@users.noreply.github.com>
2021-08-29 08:08:02 +0800

    Update BaseOSS.java

--- src/main/base/top/glowmonster/base/oss/BaseOSS.java
+++ src/main/base/top/glowmonster/base/oss/BaseOSS.java
@@ -7,8 +7,8 @@ public class BaseOSS {
     public OSS bulidOSSClient() {
         final String END_POINT = "http://oss-cn-hangzhou.aliyuncs.com";
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        final String ACCESS_KEY_ID = "LTAI4FuivYqGr2TLEQZg6QTf";
-        final String ACCESS_KEY_SECRET = "zki7KzzgUPnzHse194mmELl9Jk1Xk1";
+        final String ACCESS_KEY_ID = "";
+        final String ACCESS_KEY_SECRET = "";
         return new OSSClientBuilder().build(END_POINT, ACCESS_KEY_ID, ACCESS_KEY_SECRET);
     }
 }
