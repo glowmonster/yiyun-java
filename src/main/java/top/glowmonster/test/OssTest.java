0e69d7065e12d8590ee340fff4405cfdd93c644e
GlowMonster <34387623+glowmonster@users.noreply.github.com>
2021-09-08 18:12:52 +0800

    Update OssTest.java

--- src/main/java/top/glowmonster/test/OssTest.java
+++ src/main/java/top/glowmonster/test/OssTest.java
@@ -16,8 +16,8 @@ public class OssTest {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         /*String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        String accessKeyId = "LTAI4FuivYqGr2TLEQZg6QTf";
-        String accessKeySecret = "zki7KzzgUPnzHse194mmELl9Jk1Xk1";
+        String accessKeyId = "";
+        String accessKeySecret = "";
         String bucketName = "yiyunmusic";
 
         // 创建OSSClient实例。
